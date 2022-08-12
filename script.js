77d6912fc042ef359e6fd31f07cedaeabd8609b4
evanboyd31 <evan.boyd@mail.mcgill.ca>
2022-08-11 19:58:25 -0700

    Add playRound method to decide winners of each round

--- script.js
+++ script.js
@@ -9,4 +9,38 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "Rock") {
+                return "It's a tie!";
+            } else if (computerSelection === "Paper") {
+                return "You Lose! Paper beats Rock";
+            } else {
+                return "You win! Rock beats scissors"
+            }
+            break;
+        case "paper":
+            if (computerSelection === "Paper") {
+                return "It's a tie!";
+            } else if (computerSelection === "Scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else {
+                return "You win! Paper beats Rock"
+            }
+            break;
+        case "scissors":
+            if (computerSelection === "Scissors") {
+                return "It's a tie!";
+            } else if (computerSelection === "Rock") {
+                return "You Lose! Rock beats Scissors";
+            } else {
+                return "You win! Scissors beats paper"
+            }
+            break;
+        default:
+            return "Not a valid choice! Type rock, paper, or scissors.";
+            break;
+    }
+}
